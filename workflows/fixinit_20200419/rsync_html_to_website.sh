cb89b446a724d05ecb308831d56723b8f97647b8
Mike Hughes <mike@michaelchughes.com>
2020-04-20 01:11:05 -0400

    ENH updated params for fixinit runs, which match data better

--- workflows/fixinit_20200419/rsync_html_to_website.sh
+++ workflows/fixinit_20200419/rsync_html_to_website.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-LOCAL_PATH=/cluster/tufts/hugheslab/mhughe02/forecast_results/demo_20200414_fast/
-REMOTE_PATH=tuftscs:/h/mhughes/public_html/research/c19-tmc/demo_20200414_fast/
+nick=fixinit_20200419
+LOCAL_PATH=/cluster/tufts/hugheslab/mhughe02/forecast_results/$nick/
+REMOTE_PATH=tuftscs:/h/mhughes/public_html/research/c19-tmc/$nick/
 
 rsync -armPKL \
     --include="/*/dashboard.html" \
@@ -9,4 +10,16 @@ rsync -armPKL \
     --exclude="/*/*" \
     $LOCAL_PATH/ \
     $REMOTE_PATH
+
+cp params.json params.txt
+
+rsync -armPKL \
+    --include="/params.txt" \
+    --include="/params.json" \
+    --include="/config.json" \
+    --exclude="/*" \
+    ./ \
+    $REMOTE_PATH
+
+rm params.txt
 
