b8bbb86136f241c7aa83e11d88e378cd01e7928f
Cuong Nguyen <cuongnguyen@Cuongs-MacBook-Air.local>
2021-04-01 14:46:34 -0400

    updated autograd_for_poisson.py in questioning

--- cn_autograd_debugging/autograd_for_poisson.py
+++ cn_autograd_debugging/autograd_for_poisson.py
@@ -1,7 +1,9 @@
 # https://github.com/HIPS/autograd/blob/master/autograd/scipy/stats/poisson.py
 import autograd
 from autograd.scipy.stats.poisson import logpmf
+def logpmf_at_x(params,x):
+    return logpmf(x-params['loc'],params['mu'])
 params2= {'mu':2.0,'loc':5.0}
-print(logpmf(5.0-params2['loc'],params2['mu']))
-g2 = autograd.grad(logpmf)
-g2(5.0-params2['loc'],params2['mu'])
+print(logpmf_at_x(params2,5.0))
+g2 = autograd.grad(logpmf_at_x)
+g2(params2,5.0)
