4eac2951b2700b259b893ed16aeb8e6e720115c8
gvisani <gian_marco.visani@tufts.edu>
2021-04-16 11:09:27 -0400

    removed old simulator from init

--- aced_hmm/simulator/__init__.py
+++ aced_hmm/simulator/__init__.py
@@ -1,3 +1,2 @@
-from .PatientTrajectory import PatientTrajectory
 from .simulate_traj__python import simulate_traj__python, run_forecast__python
 # from .simulate_traj__cython import simulate_traj__cython, run_forecast__cython
