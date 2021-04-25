b9015fa5073cdc1096b2c5561d5b7795a1000cf5
gvisani <43044242+gvisani@users.noreply.github.com>
2021-04-25 12:23:56 -0400

    added cython extension

--- aced_hmm/simulator/__init__.py
+++ aced_hmm/simulator/__init__.py
@@ -1,2 +1,2 @@
 from .simulate_traj__python import simulate_traj__python, run_forecast__python
-# from .simulate_traj__cython import simulate_traj__cython, run_forecast__cython
+from .simulate_traj__cython import simulate_traj__cython, run_forecast__cython
